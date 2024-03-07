317b18d607a34e8dfc76724102f77b736b13197e
mb-tousif <meta.block.tousif@gmail.com>
2024-03-07 11:25:32 +0600

    Description section added

--- App.tsx
+++ App.tsx
@@ -3,61 +3,78 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Top Section */}
       <View
         style={{
           borderRadius: 10,
           marginTop: 16,
           padding: 16,
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "justify",
           backgroundColor: "#FEFEFE",
         }}
       >
-        <Image
-          source={require("./assets/Oval.png")}
-          style={{
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            marginBottom: 16,
-            margin: "auto",
-          }}
-        />
         <View
           style={{
-            alignItems: "center",
-            justifyContent: "center",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "justify",
           }}
         >
-          <Text
+          {/* Top Section */}
+          <Image
+            source={require("./assets/Oval.png")}
             style={{
-              fontSize: 20,
-              fontWeight: "bold",
+              width: 100,
+              height: 100,
+              borderRadius: 50,
+              marginBottom: 16,
+              margin: "auto",
+            }}
+          />
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
             }}
           >
-            Tousif
-          </Text>
-          <Text
-            style={{
-              fontSize: 16,
-              color: "#2479FF",
-              marginLeft: 8,
-            }}
-          >
-            Full Stack Developer
-          </Text>
-          <Text
-            style={{
-              fontSize: 16,
-              color: "#2479FF",
-              marginLeft: 8,
-            }}
-          >
-            Joined 8 Mar 2024
-          </Text>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: "bold",
+              }}
+            >
+              Tousif
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: "#2479FF",
+              }}
+            >
+              @github/mb-tousif
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                color: "#BDC5D3",
+                marginLeft: 8,
+              }}
+            >
+              Joined 8 Jan 2021
+            </Text>
+          </View>
         </View>
+        {/* Description Section */}
+        <Text
+          style={{
+            color: "#BDC5D3",
+            marginTop: 16,
+            textAlign: "justify",
+          }}
+        >
+          I am a Full Stack Developer with 3 years of experience. I have worked
+          on multiple projects and have a good understanding of the software
+          development life-cycle. I am a quick learner and have a passion for
+          learning new technologies.
+        </Text>
       </View>
     </View>
   );
